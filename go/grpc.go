4b968d87d58caa82a4a838a63c9f70d9f374c8e0
InvalidJoker <0limitdev@gmail.com>
2025-03-08 19:55:37 +0100

    feat(go): implement pubsub

---
+++ go/grpc.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+type AuthCallCredentials struct {
+	SecretKey string
+}
+
+func (a *AuthCallCredentials) GetRequestMetadata(context.Context, ...string) (map[string]string, error) {
+	return map[string]string{
+		"Authorization": a.SecretKey,
+	}, nil
+}
+
+func (a *AuthCallCredentials) RequireTransportSecurity() bool {
+	return false
+}
+
+func CreateConnection(hostAddress string, secretKey string) *grpc.ClientConn {
+	var opts []grpc.DialOption
+	opts = append(opts, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	opts = append(opts, grpc.WithPerRPCCredentials(&AuthCallCredentials{
+		SecretKey: secretKey,
+	}))
+	conn, err := grpc.NewClient(hostAddress, opts...)
+	if err != nil {
+		log.Fatalf("Failed to connect to simplecloud: %v", err)
+	}
+
+	return conn
+}
